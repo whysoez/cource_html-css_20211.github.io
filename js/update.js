f6c3afa7e458256f18d14f8c71c7c241a20cfc43
hung.dz <whysoez2312@gmail.com>
2022-08-12 17:07:09 +0700

    edit

--- js/update.js
+++ js/update.js
@@ -5,12 +5,11 @@ function start(){
     update();
 }
 
-start();
+// start();
+
+var state = 1;
 
 function update(){
-    var createBtn = document.querySelector('#update');
-
-    createBtn.onclick = function() {
         var movieId = document.querySelector('#movieId').value;
         var name = document.querySelector('#name').value;
         var genre = document.querySelector('#genre').value;
@@ -24,9 +23,17 @@ function update(){
             duration: duration,
             releaseDate: releaseDate
         };
+        test();
 
-        updateMovie(dataForm);
-    }
+        var showSate = document.querySelector(".state");
+        if(state != 5)
+        {
+            showSate.innerHTML = "Vui lòng kiểm tra lại thông tin.";
+        }
+        else {
+            updateMovie(dataForm);
+            showSate.innerHTML = `<p style="color:blue">Tạo thành công!</p>`;
+        }
 }
 
 function updateMovie(data, callback) {
@@ -42,4 +49,87 @@ function updateMovie(data, callback) {
         respone.json();
     })
     .then(callback);
+}
+
+
+function focusId() {
+    var name = document.querySelector('#movieId').value;
+    var checkname = document.querySelector('#checkid')
+    if(name == null || name.trim().length == 0)
+    {
+        checkname.innerHTML = "không được để trống Id";
+    }
+    else {
+        checkname.innerHTML = "";
+        state++;
+    }
+}
+
+function focusName() {
+    var name = document.querySelector('#name').value;
+    var checkname = document.querySelector('#checkname')
+    if(name.trim().length == 0)
+    {
+        checkname.innerHTML = "không được để trống Name";
+        // state = fasle;
+    }
+    else if(name.length < 5 || name.length > 50)
+    {
+        checkname.innerHTML = "<p>tên phải có độ dài từ 5-50 ký tự</p>";
+        // state = fasle;
+    }
+    else {
+        checkname.innerHTML = "";
+        state++;
+    }
+}
+
+
+function focusGenre() {
+    var name = document.querySelector('#genre').value;
+    var checkname = document.querySelector('#checkgenre')
+    if(name == null || name.trim().length == 0)
+    {
+        checkname.innerHTML = "không được để trống Genre";
+    }
+    else {
+        checkname.innerHTML = "";
+        state++;
+    }
+}
+
+function focusDuration() {
+    var name = document.querySelector('#duration').value;
+    var checkname = document.querySelector('#checkduration')
+    if(name == null || name.trim().length == 0)
+    {
+        checkname.innerHTML = "không được để trống Duration";
+    }
+    else {
+        checkname.innerHTML = "";
+        state++;
+    }
+}
+
+function focusReleaseDate() {
+    var name = document.querySelector('#releaseDate').value;
+    var checkname = document.querySelector('#checkreleasedate')
+    if(name == null || name.trim().length == 0)
+    {
+        checkname.innerHTML = "không được để trống releaseDate";
+    }
+    else {
+        checkname.innerHTML = "";
+        state++;
+    }
+}
+
+function test(){
+    state = 0;
+    focusId();
+    focusName();
+    focusGenre();
+    focusDuration();
+    focusReleaseDate();
+    console.log(state);
 }
